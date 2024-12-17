b3115aba31bb401faaee95397bff7e04787bb611
Kajsa Asp <kajsalisaasp@gmail.com>
2024-12-17 14:18:35 +0100

    updates in homapage test

--- frontend/cypress/component/HomePage.cy.tsx
+++ frontend/cypress/component/HomePage.cy.tsx
@@ -1,10 +1,11 @@
 import Page from '../../src/app/page';
+import { RecipeInterface } from '../../src/utils/interface';
 
 // Här används mock data från en fixtures file (recipes.json)
 // Koden nedan ska hämta de 3 recpeten som finns i json filen, en "låtsas /tillfällig databas kan man säga" Detta kallas mockning.
 
 describe('<Page/>', () => {
-	it('renders 3 recipes', () => {
+	it('renders recipes', () => {
 		// Ladda data från fixtures
 		cy.fixture('recipes').then((json) => {
 			// Mocka GET-anropet
@@ -21,7 +22,7 @@ describe('<Page/>', () => {
 		cy.get('span').then(($span) => {
 			const spanValue = $span.text(); // Hämtar textvärdet från span
 			cy.log(spanValue); // Loggar värdet för att verifiera om värdet är 3 så stämmer allt eftersom jag skapade 3 recept i jsonfilen :)
-			cy.fixture('recipes').then((recipes) => {
+			cy.fixture('recipes').then((recipes: RecipeInterface[]) => {
 				recipes.forEach((recipe) => {
 					cy.contains(recipe.name).should('exist');
 				});
