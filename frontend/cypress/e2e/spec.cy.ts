1aaa51df42109a5e092cb9c438524d42bf05c778
Kajsa Asp <kajsalisaasp@gmail.com>
2024-12-18 19:40:03 +0100

    test

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -13,6 +13,7 @@ describe('Create new recipe', function () {
 	it('Should retrieve the list of recipes and verify the counts', function () {
 		cy.visit('/');
 		cy.intercept('GET', 'http://localhost:3000/api/recipes').as('getRecipes');
+		cy.wait('@getRecipes');
 		cy.wait(1000);
 		cy.get('p').should('exist');
 		cy.get('span')
@@ -28,14 +29,14 @@ describe('Create new recipe', function () {
 		cy.visit('/addRecipe');
 		cy.get('form').should('be.visible');
 		cy.get('#title').type('Spagetti med köttfärsås');
-		// cy.fixture('image5.jpg', 'base64').then((fileContent) => {
-		// 	cy.get('input[type="file"]').attachFile({
-		// 		fileContent,
-		// 		fileName: 'image5.jpg',
-		// 		mimeType: 'image/jpeg',
-		// 		encoding: 'base64',
-		// 	});
-		// });
+		cy.fixture('image5.jpg', 'base64').then((fileContent) => {
+			cy.get('input[type="file"]').attachFile({
+				fileContent,
+				fileName: 'image5.jpg',
+				mimeType: 'image/jpeg',
+				encoding: 'base64',
+			});
+		});
 		cy.get('#cookTime').type('30');
 		cy.get('#category').select('Pasta');
 		cy.get('#servings').select('3');
@@ -91,31 +92,31 @@ describe('Create new recipe', function () {
 });
 
 // integrationstest
-it('Check if posting (mockad) data works using a integretion test', function () {
-	cy.visit('/');
-	cy.wait(1000); // Vänta på get-anropet
-	const newRecipe = {
-		name: 'Kyckling Alfredo',
-		cook_time: '25 min',
-		description: 'En krämig och god kyckling Alfredo med pasta och parmesan.',
-		img_url: 'image4.jpg',
-		servings: 3,
-		course_id: 3,
-		instructions:
-			'["Halvera kycklingfiléerna till 4 tunna filéer.","Vänd i mjölet, salta och peppra.","Finhacka schalottenlök och vitlök.","Fräs mjukt i en stekpanna på medelhög värme tills löken blir mjuk och glansig.","Tillsätt färskost, grädde och kycklingfond. Låt puttra ihop.","Stek kycklingen gyllene i smör.","Vänd ned riven parmesan i såsen.","Skär kycklingen i snygga skivor.","Blanda pastan med såsen och lägg kycklingen ovanpå vid servering.","Toppa med finhackad färsk persilja."]',
-		ingredients:
-			'["400 g tagliatelle", "2 kycklingfiléer", "0,5 dl vetemjöl","1 schalottenlök"," 2 klyftor vitlök", "2 msk smör, att steka löken i","100 g färskost", "3 dl vispgrädde (eller matlagningsgrädde)","1,5-2 msk koncentrerad kycklingfond","1 dl parmesanost, riven", "salt", "svartpeppar"]',
-	};
-	cy.request('POST', 'http://localhost:3000/api/new-recipe2', newRecipe).then(
-		(response) => {
-			expect(response.status).to.eq(201);
-		}
-	);
-	cy.request('GET', 'http://localhost:3000/api/recipes').then((response) => {
-		const recipes = response.body;
-		cy.log('Recived recipes:', recipes);
-	});
-});
+// it('Check if posting (mockad) data works using a integretion test', function () {
+// 	cy.visit('/');
+// 	cy.wait(1000); // Vänta på get-anropet
+// 	const newRecipe = {
+// 		name: 'Kyckling Alfredo',
+// 		cook_time: '25 min',
+// 		description: 'En krämig och god kyckling Alfredo med pasta och parmesan.',
+// 		img_url: 'image4.jpg',
+// 		servings: 3,
+// 		course_id: 3,
+// 		instructions:
+// 			'["Halvera kycklingfiléerna till 4 tunna filéer.","Vänd i mjölet, salta och peppra.","Finhacka schalottenlök och vitlök.","Fräs mjukt i en stekpanna på medelhög värme tills löken blir mjuk och glansig.","Tillsätt färskost, grädde och kycklingfond. Låt puttra ihop.","Stek kycklingen gyllene i smör.","Vänd ned riven parmesan i såsen.","Skär kycklingen i snygga skivor.","Blanda pastan med såsen och lägg kycklingen ovanpå vid servering.","Toppa med finhackad färsk persilja."]',
+// 		ingredients:
+// 			'["400 g tagliatelle", "2 kycklingfiléer", "0,5 dl vetemjöl","1 schalottenlök"," 2 klyftor vitlök", "2 msk smör, att steka löken i","100 g färskost", "3 dl vispgrädde (eller matlagningsgrädde)","1,5-2 msk koncentrerad kycklingfond","1 dl parmesanost, riven", "salt", "svartpeppar"]',
+// 	};
+// 	cy.request('POST', 'http://localhost:3000/api/new-recipe2', newRecipe).then(
+// 		(response) => {
+// 			expect(response.status).to.eq(201);
+// 		}
+// 	);
+// 	cy.request('GET', 'http://localhost:3000/api/recipes').then((response) => {
+// 		const recipes = response.body;
+// 		cy.log('Recived recipes:', recipes);
+// 	});
+// });
 
 // it('Check if it is possible to delete a recipe', function () {
 // 	cy.visit('/');
